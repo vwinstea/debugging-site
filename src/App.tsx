32b50a3d46c0e818075bce5949891f38729c9ab6
Vance Winstead <vance.winstead@gmail.com>
2022-07-05 17:48:07 -0700

    Added prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <p>
-      Cleanedup React App
-    </p>
-  );
+  return <p>Cleanedup React App</p>;
 }
 
 export default App;
